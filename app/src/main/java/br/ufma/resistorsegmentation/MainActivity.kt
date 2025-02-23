bd016ff425d1fbd1e4e9a326ccd9f358e512bb87
Luan <barkon32@tuta.io>
2025-02-22 21:45:36 -0300

    feat: set up main activity class

--- app/src/main/java/br/ufma/resistorsegmentation/MainActivity.kt
+++ app/src/main/java/br/ufma/resistorsegmentation/MainActivity.kt
@@ -1,47 +1,59 @@
 package br.ufma.resistorsegmentation
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Size
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.ufma.resistorsegmentation.ui.theme.ResistorSegmentationTheme
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.camera.core.Preview
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
+    private lateinit var previewView: PreviewView
+    private lateinit var overlayView: SegmentationOverlayView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ResistorSegmentationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        previewView = findViewById(R.id.preview_view)
+        overlayView = findViewById(R.id.overlay_view)
+
+        // Request camera permission
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+        } else {
+            startCamera()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ResistorSegmentationTheme {
-        Greeting("Android")
+            // Preview use case
+            val preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_DEFAULT) // Match model input, e.g., 640x640
+                .build()
+                .also { it.setSurfaceProvider(previewView.surfaceProvider) }
+
+            // Image analysis use case
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(Size(640, 640)) // Match model input size
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also { it.setAnalyzer(ContextCompat.getMainExecutor(this), Analyzer(overlayView)) }
+
+            // Bind to lifecycle
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalysis)
+        }, ContextCompat.getMainExecutor(this))
     }
 }
